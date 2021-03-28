063857fb487c54036b30be93a4d54745bc81d844
U-DESKTOP-36D07L1\kelly <kkwang@andrew.cmu.edu>
2021-03-28 19:56:54 -0400

    Added Gallery and Resources sections (examples simply listed)

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -22,43 +22,23 @@ const Resources = () => (
 	    </div>
 	</a>
 	<h4>Example Posters from Past Years:</h4>
-        <div id="carouselExamples" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExamples" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExamples" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExamples" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#carouselExamples" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#carouselExamples" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                <button type="button" data-bs-target="#carouselExamples" data-bs-slide-to="5" aria-label="Slide 6"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-		    <img className="poster" src={poster1} alt={alt1}/>
-                </div>
-                <div class="carousel-item">
-		    <img className="poster" src={poster2} alt={alt2}/>
-                </div>
-                <div class="carousel-item">
-		    <img className="poster" src={poster3} alt={alt3}/>
-                </div>
-                <div class="carousel-item">
-		    <img className="poster" src={poster4} alt={alt4}/>
-                </div>
-                <div class="carousel-item">
-		    <img className="poster" src={poster5} alt={alt5}/>
-                </div>
-                <div class="carousel-item">
-		    <img className="poster" src={poster6} alt={alt6}/>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExamples" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExamples" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+        <div class="poster-row">
+	    <img className="poster" src={poster1} alt={alt1}/>
+        </div>
+        <div class="poster-row">
+	    <img className="poster" src={poster2} alt={alt2}/>
+        </div>
+        <div class="poster-row">
+	    <img className="poster" src={poster3} alt={alt3}/>
+        </div>
+        <div class="poster-row">
+	    <img className="poster" src={poster4} alt={alt4}/>
+        </div>
+        <div class="poster-row">
+	    <img className="poster" src={poster5} alt={alt5}/>
+        </div>
+        <div class="poster-row">
+	    <img className="poster" src={poster6} alt={alt6}/>
         </div>
     </section>
 )
