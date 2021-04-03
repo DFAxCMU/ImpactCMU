8dbadb17f685c5c851234ba460e209f262bc101d
Sashank Gogula <sashankg@gmail.com>
2021-04-03 16:43:47 -0400

    Added basic carousel for gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import slide from '../images/test_slide.jpg';
+import { Carousel } from 'react-bootstrap';
 
 const Gallery = () => (
     <section id="gallery">
         <h1>Gallery</h1>
-	<h3>Coming Soon!</h3>
+        <div style={{ display: 'flex', flexWrap: 'wrap', width: 848}}>
+            { [1, 2, 3, 4, 5, 6, 7].map(i => {
+                return <Carousel 
+                    style={{ width: 400, height: 400, margin: 12 }}
+                    indicators={ true }
+                    interval={ 0 }
+                >
+                    <Carousel.Item>
+                        <img width="100%" height="100%" src={ slide } alt="" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width="100%" height="100%" src={ slide } alt="" />
+                    </Carousel.Item>
+                </Carousel>
+            })}
+        </div>
     </section>
 )
 
