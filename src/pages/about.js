60b312c0a864e8f8e404e889a6947ff2e6ec1f1c
erica-w-fu <efu@andrew.cmu.edu>
2022-03-17 23:43:46 -0400

    created empty green box cards

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,20 @@ import '../dist/css/bootstrap.min.css'
 //import '../dist/js/bootstrap.bundle.js'
 import '../styles/style.css'
 import '../styles/page.css'
-import Contact from '../components/Contact';
+import Navbar from '../components/Navbar';
+import Card from '../components/Card'
 
 const AboutPage = () => (
     <div>
-        <Contact />
+        <Navbar />
+        <div className="body">
+            <Card 
+                title="About"
+                body="Organized by Design for America, the goal of Impact CMU is to celebrate our successes and to create a culture of social good on campus."
+                bodyClass="large"
+            />
+            
+        </div>
     </div>
 )
 
