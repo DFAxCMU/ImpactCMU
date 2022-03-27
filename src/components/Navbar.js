2798dc853f1d4697845e4e6600d7743209146da5
Kely Wang <kkwang@andrew.cmu.edu>
2022-03-27 12:28:27 -0400

    created basic structure of hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import { 
-    Navbar as N, 
-    Nav
-} from 'react-bootstrap';
+import React, {useState} from 'react'
 
 import Contact from './Contact.js'
+import hamburgerIcon from '../images/hamburger-menu.svg'
 
-class Navbar extends React.Component {
-    render() {
-        return (
-            <>
-                <div id="navbar">
-                    <a className="logo green-box" href="/">
-                        IMPACT CMU
-                    </a>
-                    <Contact />
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <>
+            <div id="navbar">
+                <a className="logo green-box" href="/">
+                    IMPACT CMU
+                </a>
+                <Contact />
+                <div onClick={() => setIsOpen(!isOpen)}>
+                    <img className="hamburger" src={ hamburgerIcon } alt="menu icon"/>
                 </div>
-            </>
-        )
-    }
+                { isOpen ?
+                <div className="open-menu">
+                    <a className="menu-item" href="/about">About</a>
+                    <a className="menu-item" href="/timeline">Timeline</a>
+                    <a className="menu-item" href="/resources">Resources</a>
+                    <a className="menu-item" href="https://forms.gle/LMyKcb1ctstv5XUN6">Submit a Project</a>
+                    <a className="menu-item" href="/projects">Past Impact</a>
+                </div>
+                :
+                <div></div>
+                }
+            </div>
+        </>
+    )
 } 
 
 export default Navbar
