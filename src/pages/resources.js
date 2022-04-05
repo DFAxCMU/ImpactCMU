c81fbc5031886a591b573ba8f40e71bada7316fe
Kely Wang <kkwang@andrew.cmu.edu>
2022-04-05 19:26:37 -0400

    added resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -24,11 +24,24 @@ const ResourcesPage = () => (
             <link rel="icon" type="image/png" sizes="16x16" href={ favicon32 } />
         </Helmet>
         <Navbar />
-        <Card
-            title='Coming Soon!'
-            tilt='tilt-c'
+        <Card 
+            title="Resources"
+            tilt="tilt-c"
         />
-        <Contact/>
+        <div className="body">
+            <div className="summary">
+            <h3>
+                For participating projects, check out the slides from our presenters workshop or the project poster template provided for your convenience!
+            </h3>
+            <h3>
+                Make sure to submit your poster before the submission deadline, and we'll handle the printing.
+            </h3>
+            </div>
+            <div className="nav-grid">
+                <a className="grid-item green-box" href="https://docs.google.com/presentation/d/1dU9skh6-5CM-VSSUwPlrhZl1r0R7ZgpBos3oBh0aqOM/edit#slide=id.g12060692642_0_0">Workshop Slides</a>
+                <a className="grid-item green-box" href="https://www.figma.com/file/le5xvR8H4ya6Aa5GmyyYLY/Impact-CMU-Poster-Template?node-id=0%3A1">Poster Template</a>
+            </div>
+        </div>
     </div>
 )
 
