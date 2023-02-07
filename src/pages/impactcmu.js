bb3de5cb6c2f290cf04bfd66fc1f1e8c4cca5e07
erica-w-fu <efu@andrew.cmu.edu>
2023-02-07 01:05:51 -0500

    added email to the footer

--- src/pages/impactcmu.js
+++ src/pages/impactcmu.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import {graphql} from 'gatsby'
 
 import 'popper.js'
 import '../dist/css/bootstrap.min.css'
@@ -14,65 +12,14 @@ import Gallery from '../components/Gallery';
 import Footer from '../components/Footer';
 
 
-import Logistics from '../components/Logistics';
-import Apply from '../components/Apply';
-import Prizes from '../components/Prizes';
-import Resources from '../components/Resources';
-import Contact from '../components/Contact';
-
-// import schedule from '../data/timeline.csv'
-
-
-import {
-    Jumbotron,
-    Button,
-    ButtonGroup,
-    Container,
-    Row,
-    Col
-} from 'react-bootstrap';
-
-const impactBlue = '#04285a';
-const impactLightBlue = '#2b7095';
-const impactRed = '#c44536';
-const impactYellow = '#fce4b5';
-const impactBrown = '#772e25';
-
-const ImpactCMU = ({ data }) => (
+const ImpactCMU = () => (
     <div>
         <Hero />
         <About />
         <Timeline />
         <Gallery />
         <Footer />
-        
-        {/* <Prizes />
-        <Apply />
-        <Logistics data={ data.allTimelineCsv } />
-        <Resources />
-        <Contact /> */}
     </div>
 )
 
 export default ImpactCMU
-
-export const query = graphql`
-    query ImageQuery {
-        allFile(filter:{relativeDirectory:{eq:"orgs"}}) {
-            edges{
-                node{
-                    relativePath,
-                    publicURL
-                }
-            }
-        }
-      allTimelineCsv {
-        edges {
-            node {
-                time
-                name
-                detail
-            }
-        }
-      }
-}`
