e5362e2e6356a650c326573c346468a56a2c5c00
chelstang <86565244+chelstang@users.noreply.github.com>
2023-02-13 18:19:52 -0500

    Updated timeline (moved time)

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -11,12 +11,12 @@ const Timeline = () => (
                 <p><a href="https://forms.gle/YTnNBSFBfKpLqC8Y8" target="_blank">Project submission form</a> due!</p>
             </div>
             <div className="two-col">
-                <h2>3/26 @ 2-3PM</h2>
-                <p>Mandatory information session</p>
+                <h2>3/26</h2>
+                <p>Mandatory information session @2-3PM</p>
             </div>
             <div className='two-col'>
-                <h2>4/2 @ 1-2PM</h2>
-                <p>Optional poster session</p>
+                <h2>4/2</h2>
+                <p>Optional poster session @1-2PM</p>
             </div>
             <div className='two-col'>
                 <h2>4/9</h2>
