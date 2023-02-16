899523d25d0268225a551dcaef7cff43547e3416
chelstang <86565244+chelstang@users.noreply.github.com>
2023-02-16 14:50:36 -0500

    SEO Keywords

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
         title: `ImpactCMU`,
         pathPrefix: `/ImpactCMU`,
         description: `Impact CMU 2023 is an annual showcase of CMU student-led social impact projects. Organized by Design for America, the goal of Impact CMU is to celebrate our successes and to create a culture of social good on campus.`,
+        keywords: `impact, carnegie mellon, CMU, DFA, design for dmerica, impactcmu`,
     },
     plugins: [
         {
