8c8fc1702d4d519ca7199c1598db9eb30ba62f27
Christina Moon <christinajmoon@gmail.com>
2024-02-18 16:55:19 -0500

    fix navabr on pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,30 +11,33 @@ const Hero = () => {
     };
 
     return (
-      <section id="about">
-        <div className="content">
-          <Draggable defaultPosition={{x: 350, y: 75}}>
-            <div className="draggable-box">
-              <h2 class = "box-title">About</h2>
-              <p class = "box-text">Impact CMU 2024 is an end of year showcase of CMU student-led social impact projects.</p>
-              <div className="ratio ratio-16x9">
-                <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+      <>
+        <NavBarHero />
+        <section id="about">
+          <div className="content">
+            <Draggable defaultPosition={{x: 350, y: 75}}>
+              <div className="draggable-box">
+                <h2 class = "box-title">About</h2>
+                <p class = "box-text">Impact CMU 2024 is an end of year showcase of CMU student-led social impact projects.</p>
+                <div className="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                </div>
               </div>
+            </Draggable>
+            <Draggable defaultPosition={{x: -400, y: 100}}>
+              <div className="draggable-box">
+                <p class = "box-text">Enter in your social good project for a change to win $250!</p>
+                <center>
+                  <button onClick={() => handleButtonClick('https://docs.google.com/forms/d/e/1FAIpQLSdXLtbzD7CaYo_54JB5bKdsDMDsQTxrx_Bg5YPVCwCc8WtdZQ/closedform')}>Submit</button>
+                </center>
+              </div>
+            </Draggable>
+            <div className="sketch-iframe">
+              <iframe src="https://openprocessing.org/sketch/2178953/embed/" width="2000px" height="1200px"></iframe>
             </div>
-          </Draggable>
-          <Draggable defaultPosition={{x: -400, y: 100}}>
-            <div className="draggable-box">
-              <p class = "box-text">Enter in your social good project for a change to win $250!</p>
-              <center>
-                <button onClick={() => handleButtonClick('https://docs.google.com/forms/d/e/1FAIpQLSdXLtbzD7CaYo_54JB5bKdsDMDsQTxrx_Bg5YPVCwCc8WtdZQ/closedform')}>Submit</button>
-              </center>
-            </div>
-          </Draggable>
-          <div className="sketch-iframe">
-            <iframe src="https://openprocessing.org/sketch/2178953/embed/" width="2000px" height="1200px"></iframe>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
     );
   };
 
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,41 +3,44 @@ import schedule from './2024img/Schedule.png';
 import NavBar from "../components/NavBar";
 
 const Hero = () => (
-    <section id="schedule" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <div className="container-fluid">
-            <div className=" text-center">
-                <a className="navbar-brand" href="/home">
-                    <img src={schedule} alt="title-img" class="title-img"/>
-                </a>
-            </div>
-            <div className="boxes1" style={{ display: 'flex'}}> 
-                <div class="row1">
-                    <div className="box1">
-                        <h3>Project Showcase</h3>
-                        <h3 class = "time">12-5PM</h3>
-                        <p>View impactful projects from our participants and learn about their work.</p>
+    <>
+        <NavBar />
+        <section id="schedule" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div className="container-fluid">
+                <div className=" text-center">
+                    <a className="navbar-brand" href="/home">
+                        <img src={schedule} alt="title-img" class="title-img"/>
+                    </a>
+                </div>
+                <div className="boxes1" style={{ display: 'flex'}}> 
+                    <div class="row1">
+                        <div className="box1">
+                            <h3>Project Showcase</h3>
+                            <h3 class = "time">12-5PM</h3>
+                            <p>View impactful projects from our participants and learn about their work.</p>
+                        </div>
+                        <div className="box1">
+                            <h3>Voting Closes</h3>
+                            <h3 class = "time">4:30PM</h3>
+                            <p>Enter into raffles to earn prizes and eat free pizza and popcorn!</p>
+                        </div>
                     </div>
-                    <div className="box1">
-                        <h3>Voting Closes</h3>
-                        <h3 class = "time">4:30PM</h3>
-                        <p>Enter into raffles to earn prizes and eat free pizza and popcorn!</p>
+                    <div class="row2">
+                        <div className="box1">
+                            <h3>Kickoff Ceremony</h3>
+                            <h3 class = "time">12PM</h3>
+                            <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
+                        </div>
+                        <div className="box1">
+                            <h3>Closing Ceremony</h3>
+                            <h3 class = "time">5PM</h3>
+                            <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="row2">
-                    <div className="box1">
-                        <h3>Kickoff Ceremony</h3>
-                        <h3 class = "time">12PM</h3>
-                        <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
-                    </div>
-                    <div className="box1">
-                        <h3>Closing Ceremony</h3>
-                        <h3 class = "time">5PM</h3>
-                        <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
-                    </div>
-                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </>
 )
 
 export default Hero;
