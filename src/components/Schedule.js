46bf6c613ba63619e1c9400ba01c031c480444cc
Christina Moon <christinajmoon@gmail.com>
2024-02-18 17:13:22 -0500

    schedule changes (wording + spacing)

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -16,26 +16,26 @@ const Hero = () => (
                     {/* <div class="row1"> */}
                     <div className="timeline-container">
                         <div className="box3">
-                            <h3>Project Showcase</h3>
-                            <h3 class = "time">12-5PM</h3>
-                            <p>View impactful projects from our participants and learn about their work.</p>
-                        </div>
-                        <div className="box3">
-                            <h3>Voting Closes</h3>
-                            <h3 class = "time">4:30PM</h3>
-                            <p>Enter into raffles to earn prizes and eat free pizza and popcorn!</p>
-                        </div>
-                    {/* </div> */}
-                    {/* <div class="row2"> */}
-                        <div className="box3">
                             <h3>Kickoff Ceremony</h3>
                             <h3 class = "time">12PM</h3>
                             <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
                         </div>
                         <div className="box3">
+                            <h3>Project Showcase</h3>
+                            <h3 class = "time">12-4PM</h3>
+                            <p>View impactful projects from our participants and learn about their work.</p>
+                        </div>
+                    {/* </div> */}
+                    {/* <div class="row2"> */}
+                        <div className="box3">
+                            <h3>Voting Closes</h3>
+                            <h3 class = "time">4PM</h3>
+                            <p>Enter into raffles to earn prizes and eat free pizza and popcorn!</p>
+                        </div>
+                        <div className="box3">
                             <h3>Closing Ceremony</h3>
-                            <h3 class = "time">5PM</h3>
-                            <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
+                            <h3 class = "time">4:30PM</h3>
+                            <p>Listen to the closing ceremony. Project voting ends.</p>
                         </div>
                     </div>
                     {/* </div> */}
