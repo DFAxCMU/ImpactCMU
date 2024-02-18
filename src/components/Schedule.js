14d6e2e191dc33b54af27249d1f2e0307a27fbc5
Christina Moon <christinajmoon@gmail.com>
2024-02-18 17:06:54 -0500

    edits on schedule page (margin/padding)

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -13,30 +13,32 @@ const Hero = () => (
                     </a>
                 </div>
                 <div className="boxes1" style={{ display: 'flex'}}> 
-                    <div class="row1">
-                        <div className="box1">
+                    {/* <div class="row1"> */}
+                    <div className="timeline-container">
+                        <div className="box3">
                             <h3>Project Showcase</h3>
                             <h3 class = "time">12-5PM</h3>
                             <p>View impactful projects from our participants and learn about their work.</p>
                         </div>
-                        <div className="box1">
+                        <div className="box3">
                             <h3>Voting Closes</h3>
                             <h3 class = "time">4:30PM</h3>
                             <p>Enter into raffles to earn prizes and eat free pizza and popcorn!</p>
                         </div>
-                    </div>
-                    <div class="row2">
-                        <div className="box1">
+                    {/* </div> */}
+                    {/* <div class="row2"> */}
+                        <div className="box3">
                             <h3>Kickoff Ceremony</h3>
                             <h3 class = "time">12PM</h3>
                             <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
                         </div>
-                        <div className="box1">
+                        <div className="box3">
                             <h3>Closing Ceremony</h3>
                             <h3 class = "time">5PM</h3>
                             <p>Listen to the opening ceremony with a special guest speaker. Project voting starts.</p>
                         </div>
                     </div>
+                    {/* </div> */}
                 </div>
             </div>
         </section>
