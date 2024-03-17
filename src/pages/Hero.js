7fdb9e5c7b4e47307d22d0544a83e9393ffdd093
erica-w-fu <efu@andrew.cmu.edu>
2024-03-17 14:22:40 -0400

    Mobile hero page changed. Needs fixing with desktop version

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Draggable from "react-draggable";
+import { useState, useEffect } from "react";
+
 import '../dist/css/bootstrap.min.css';
 import NavBarHero from "../components/NavBarHero";
 import Nav from "react-bootstrap/Nav";
@@ -21,23 +23,35 @@ const Hero = () => {
         <NavBarHero />
         <section id="about">
           <div className="content">
-            <Draggable defaultPosition={{x: 350, y: -36}}>
+            {/* <Draggable defaultPosition={{x: 350, y: -36}}>
               <div className="draggable-box">
                 <h2 class = "box-title">About</h2>
                 <p class = "box-text">Impact CMU 2024 is an end of year showcase of CMU student-led social impact projects.</p>
               </div>
-            </Draggable>
-            <Draggable defaultPosition={{x: -400, y: 64}}>
+            </Draggable> */}
+            {/* <Draggable defaultPosition={{x: -400, y: 64}}>
               <div className="draggable-box">
                 <p class = "box-text">Enter in your social good project for a chance to win $250!</p>
                 <center>
                   <button><Nav.Link href="/Submit">Submit</Nav.Link></button>
                 </center>
               </div>
-            </Draggable>
+            </Draggable> */}
+             <div className="draggable-box1">
+                {/* <h2 class = "box-title">About</h2> */}
+                <p class = "box-text">Impact CMU 2024 is an end of year showcase of CMU student-led social impact projects.</p>
+            </div>
+
+            <div className="draggable-box2">
+                <p class = "box-text">Enter in your social good project for a chance to win $250!</p>
+                <center>
+                  <button class="submit-button"><Nav.Link href="/Submit">Submit</Nav.Link></button>
+                </center>
+            </div>
+
             <div className="sketch-iframe">
-              <iframe src="https://openprocessing.org/sketch/2185428/embed/" width="2400px" height="1224px"></iframe>
-              {/* <iframe src="https://openprocessing.org/sketch/2178967/embed/" width="2400px" height="1224px"></iframe> */}
+              {/* <iframe src="https://openprocessing.org/sketch/2185428/embed/" width="2400px" height="1224px"></iframe> */}
+              <iframe src="https://openprocessing.org/sketch/2187973/embed/" width="440" height="1900"></iframe>
             </div>
           </div>
         </section>
