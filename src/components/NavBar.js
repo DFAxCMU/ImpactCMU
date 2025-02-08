a4d58dcf3cf5fe023705ad2a71137896fb4c052d
Christina <cmoon2@andrew.cmu.edu>
2025-02-08 10:08:23 -0800

    changes for master

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,7 +44,7 @@ export default function TopBar(props) {
                         <Nav.Link href="/About" className="hover-underline-animation">ABOUT</Nav.Link>
                         <Nav.Link href="/Submit" className="hover-underline-animation">SUBMIT</Nav.Link>
                         <Nav.Link href="/Schedule" className="hover-underline-animation">SCHEDULE</Nav.Link>
-                        <Nav.Link href="/Projects" className="hover-underline-animation">PROJECTS</Nav.Link>
+                        {/* <Nav.Link href="/Projects" className="hover-underline-animation">PROJECTS</Nav.Link> */}
                         <Nav.Link href="https://dfaxcmu.notion.site/ecfaf84847754757bf0809df74a3253c?v=087d30146f574a93bc8b935ecb0f4560&pvs=4" className="hover-underline-animation" target="_blank_">
                             2024 ARCHIVE
                         </Nav.Link>
@@ -116,22 +116,35 @@ export default function TopBar(props) {
                     </button>
                 </div>
                 <ul className={menuClicked ? 'nav-menu active' : 'nav-menu'}>
-                    <button className="menu-icon" type="button" onClick={ () => setMenuClicked((!menuClicked)) }>
+                    {/* <button className="menu-icon" type="button" onClick={ () => setMenuClicked((!menuClicked)) }>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                    </button>
+                    </button> */}
                     <div className="nav-vertical">
-                        <Nav.Link href="/About" className="hover-underline-animation">ABOUT</Nav.Link>
-                        <Nav.Link href="/Submit" className="hover-underline-animation">SUBMIT</Nav.Link>
-                        <Nav.Link href="/Projects" className="hover-underline-animation">PROJECTS</Nav.Link>
-                        <Nav.Link href="/Schedule" className="hover-underline-animation">SCHEDULE</Nav.Link>
-                        <Nav.Link href="https://dfaxcmu.notion.site/ecfaf84847754757bf0809df74a3253c?v=087d30146f574a93bc8b935ecb0f4560&pvs=4" className="hover-underline-animation" target="_blank_">
-                            2023 impact archive
+                        <div className="top-bar-small top-bar">
+                            <a href="/">
+                                <div className="logo">
+                                    <img src={logo} alt="logo" className="logo"/>
+                                </div>
+                            </a>
+                            <button className="menu-icon" type="button" onClick={ () => setMenuClicked((!menuClicked)) }>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                            </button>
+                        </div>
+                        {/* <a className="navbar-brand" href="/Hero">
+                            <img src={logo} alt="logo" className="logo"/>
+                        </a> */}
+                        <Nav.Link href="/About" className="navtext">ABOUT</Nav.Link>
+                        <Nav.Link href="/Submit" className="navtext">SUBMIT</Nav.Link>
+                        {/* <Nav.Link href="/Projects">PROJECTS</Nav.Link> */}
+                        <Nav.Link href="/Schedule" className="navtext">SCHEDULE</Nav.Link>
+                        <Nav.Link href="https://dfaxcmu.notion.site/ecfaf84847754757bf0809df74a3253c?v=087d30146f574a93bc8b935ecb0f4560&pvs=4" className="navtext" target="_blank_">
+                            2024 ARCHIVE
                         </Nav.Link>
-                        <div className="bottom">
+                        {/* <div className="bottom">
                             <a className="navbar-brand-mobile" href="/Hero">
                                 <img src={logo} alt="logo" className="logo"/>
                             </a>
-                        </div>
+                        </div> */}
                     </div>
                 </ul>
             </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,80 +1,141 @@
-import React, { useState } from 'react';
-import about from '../components/2024img/About.png';
+import React, { useState, useEffect } from 'react';
 import yellowcircle from '../components/2025img/yellowcircle.svg';
 import redcircle from '../components/2025img/redcircle.svg';
 import NavBar from "../components/NavBar";
 
-import 'popper.js'
+import 'popper.js';
 import '../dist/css/bootstrap.min.css';
 import '../styles/style.css';
 import '../styles/page.css';
 import '../styles/fonts.css';
 
 const About = () => {
-    const [openDropdown, setOpenDropdown] = useState(null);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-    const toggleDropdown = (dropdownId) => {
-        if (openDropdown === dropdownId) {
-            setOpenDropdown(null);
-        } else {
-            setOpenDropdown(dropdownId);
-        }
-        console.log(openDropdown);
-    };
-    
-    return ( 
-    <>
-        <NavBar/>
-        <section id="about">
-            <div className="container-about">
-                <div className="yellowcircle-section">
-                    <img src={yellowcircle} alt="title-img" class="yellowcircle"/>  
-                    <div className="yellowcircle-text">
-                        <h3>YO WTF</h3>
-                        <h3 classname="aboutHDR">WHO WE ARE</h3>  
-                        <p>Impact CMU invites undergrad and graduate students at Carnegie Mellon
-                            to showcase how they have worked with a community. Student organizations and research, 
-                            personal, and hackathon projects are all welcome to contribute their work!</p>
-                    </div>
-                </div>
-                <div className="redcircle-section">
-                    <img src={redcircle} alt="title-img" class="redcircle"/>  
-                    <div className="redcircle-text"> 
-                        <h3 classname="aboutHDR">WHAT IS IMPACT?</h3>  
-                        <p>You will be showcasing your work through a poster and optional demos. 
-                            Attendees will view different teams, talk to you about your work, and vote to win prizes!
-                            Faculty judges will also take part in viewing work and discussing your creations! This is a
-                            great opportunity to talk to your peers, get feedback from your professors, and for student 
-                            organizations to promote to a large audience.</p> 
-                        <h3 classname="aboutHDR">WHERE IS IT?</h3> 
-                        <p>March 3rd @ LOCATION, TIME</p> 
-                        </div>
-                </div>
-                {/* <div className=" text-center">
-                    <div className="navbar-brand">
-                        <img src={about} alt="title-img" class="title-img"/>
-                    </div>
-                </div>
-                <div class="boxes">
-                    <div class="inner-box">
-                        <div className="box" onClick={() => toggleDropdown('who')}>
-                            <h3>Who</h3>
-                            <p className={`content1 ${openDropdown === 'who' ? 'open' : ''}`}>HI CMU invites undergrad and graduate students at Carnegie Mellon to showcase how they have worked with a community. Student organizations and research, personal, and hackathon projects are all welcome to contribute their work!</p>
-                        </div>
-                        <div class="box" onClick={() => toggleDropdown('what')}> 
-                            <h3>What</h3>
-                            <p className={`content1 ${openDropdown === 'what' ? 'open' : ''}`}>Teams will be showcasing their work through a poster and optional demos. As an attendee, you have the opportunity to explore different projects, engage in discussions with the creators, and even participate in voting for your favorite creations to win prizes! Faculty judges will also be present, adding an extra layer of evaluation and feedback to the event. This is a fantastic chance to mingle with your peers, learn about innovative projects, receive valuable feedback from professors, and discover opportunities offered by student organizations. Don't miss out on this dynamic event where creativity and innovation collide!</p>
-                        </div>
-                        <div class="box" onClick={() => toggleDropdown('where')}>
-                            <h3>Where</h3>
-                            <p className={`content1 ${openDropdown === 'where' ? 'open' : ''}`}>April 11 @ Cohon University Center, 12-4PM</p>
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsSmallScreen(window.innerWidth < 769);
+        };
+
+        // check the screen size on component mount
+        checkScreenSize();
+
+        // event listener for window resize
+        window.addEventListener('resize', checkScreenSize);
+
+        // unnecessary cleanup :P
+        return () => window.removeEventListener('resize', checkScreenSize);
+    }, []);
+
+    return (
+        <>
+            <NavBar />
+            {isSmallScreen ? (
+                <div className="container-about">
+                    <div className="yellowcircle-section">
+                        <img src={yellowcircle} alt="title-img" className="yellowcircle" />
+                        <div className="yellowcircle-text">
+                            <h3 className="aboutHDR">WHO WE ARE</h3>
+                            <p>
+                                Impact CMU invites undergrad and graduate students at Carnegie Mellon
+                                to showcase how they have worked with a community. Student organizations and
+                                research, personal, and hackathon projects are all welcome to contribute their work!
+                            </p>
                         </div>
                     </div>
-                </div> */}
-            </div>
-        </section>
-    </>
-    )
-}
+                    <div className="redcircle-section">
+                        <img src={redcircle} alt="title-img" className="redcircle" />
+                        <div className="redcircle-text">
+                            <h3 className="aboutHDR">WHAT IS IMPACT?</h3>
+                            <p>
+                                You will be showcasing your work through a poster and optional demos. Attendees will
+                                view different teams, talk to you about your work, and vote to win prizes! Faculty judges
+                                will also take part in viewing work and discussing your creations! This is a great
+                                opportunity to talk to your peers, get feedback from your professors, and for student
+                                organizations to promote to a large audience.
+                            </p>
+                            <h3 className="aboutHDR">WHERE IS IT?</h3>
+                            <p>March 3rd @ LOCATION, TIME</p>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <section id="about">
+                    <div className="container-about">
+                        {/* <div className="yellowcircle-section"> */}
+                            <div><img src={yellowcircle} alt="title-img" className="yellowcircle" /></div>
+                            <div><img src={redcircle} alt="title-img" className="redcircle" /></div>
+                            <div>
+                                <h3 className="aboutHDR">WHO WE ARE</h3>
+                                <p>
+                                    Impact CMU invites undergrad and graduate students at Carnegie Mellon to showcase how
+                                    they have worked with a community. Student organizations and research, personal, and
+                                    hackathon projects are all welcome to contribute their work!
+                                </p>
+                            </div>
+                            <div>
+                                <h3 className="aboutHDR">WHAT IS IMPACT?</h3>
+                                <p>
+                                    You will be showcasing your work through a poster and optional demos. Attendees will
+                                    view different teams, talk to you about your work, and vote to win prizes! Faculty
+                                    judges will also take part in viewing work and discussing your creations! This is a
+                                    great opportunity to talk to your peers, get feedback from your professors, and for
+                                    student organizations to promote to a large audience.
+                                </p>
+                                <h3 className="aboutHDR">WHERE IS IT?</h3>
+                                <p>March 3rd @ LOCATION, TIME</p>
+                            {/* </div> */}
+                        {/* </div> */}
+                        {/* <div className="redcircle-section"> */}
+                            {/* <img src={redcircle} alt="title-img" className="redcircle" /> */}
+                            {/* <div className="redcircle-text">
+                                <h3 className="aboutHDR">WHAT IS IMPACT?</h3>
+                                <p>
+                                    You will be showcasing your work through a poster and optional demos. Attendees will
+                                    view different teams, talk to you about your work, and vote to win prizes! Faculty
+                                    judges will also take part in viewing work and discussing your creations! This is a
+                                    great opportunity to talk to your peers, get feedback from your professors, and for
+                                    student organizations to promote to a large audience.
+                                </p>
+                                <h3 className="aboutHDR">WHERE IS IT?</h3>
+                                <p>March 3rd @ LOCATION, TIME</p>
+                            </div> */}
+                        </div>
+                    </div>
+                </section>
+                // <section id="about">
+                //     <div className="container-about">
+                //         <div className="yellowcircle-section">
+                //             <img src={yellowcircle} alt="title-img" className="yellowcircle" />
+                //             <div className="yellowcircle-text">
+                //                 <h3 className="aboutHDR">WHO WE ARE</h3>
+                //                 <p>
+                //                     Impact CMU invites undergrad and graduate students at Carnegie Mellon to showcase how
+                //                     they have worked with a community. Student organizations and research, personal, and
+                //                     hackathon projects are all welcome to contribute their work!
+                //                 </p>
+                //             </div>
+                //         </div>
+                //         <div className="redcircle-section">
+                //             <img src={redcircle} alt="title-img" className="redcircle" />
+                //             <div className="redcircle-text">
+                //                 <h3 className="aboutHDR">WHAT IS IMPACT?</h3>
+                //                 <p>
+                //                     You will be showcasing your work through a poster and optional demos. Attendees will
+                //                     view different teams, talk to you about your work, and vote to win prizes! Faculty
+                //                     judges will also take part in viewing work and discussing your creations! This is a
+                //                     great opportunity to talk to your peers, get feedback from your professors, and for
+                //                     student organizations to promote to a large audience.
+                //                 </p>
+                //                 <h3 className="aboutHDR">WHERE IS IT?</h3>
+                //                 <p>March 3rd @ LOCATION, TIME</p>
+                //             </div>
+                //         </div>
+                //     </div>
+                // </section>
+            )}
+        </>
+    );
+};
 
 export default About;
