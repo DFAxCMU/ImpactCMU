e1c6e71db4fd808c2edcfcd4e69f29d5c0f21ad6
zh000-star <helen2191625316@outlook.com>
2026-02-07 04:56:50 -0500

    Wire Landing 2026 into routing and shared styles

--- src/components/NavBarHero.js
+++ src/components/NavBarHero.js
@@ -37,7 +37,7 @@ export default function TopBar(props) {
         <Navbar>
             <div className="top navbar navbar-expand-md navbar-light fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/Hero">
+                    <a className="navbar-brand" href="/">
                         <img src={logo} alt="logo" className="logo"/>
                     </a>
                     {/* <Nav className="inner d-flex flex-row justify-content-between w-100 container-fluid navbar-text"> */}
@@ -100,7 +100,7 @@ export default function TopBar(props) {
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                             </button>
                         </div>
-                        {/* <a className="navbar-brand" href="/Hero">
+                        {/* <a className="navbar-brand" href="/">
                             <img src={logo} alt="logo" className="logo"/>
                         </a> */}
                         <Nav.Link href="/About" className="navtext">ABOUT</Nav.Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,20 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import favicon16 from '../components/2025img/16.svg';
-import favicon32 from '../components/2025img/32.svg';
-import Hero from './Hero';
+import React from "react";
+import { Helmet } from "react-helmet";
+import favicon16 from "../components/2025img/16.svg";
+import favicon32 from "../components/2025img/32.svg";
 
-const IndexPage = ({ data }) => (
-    <div>
-        <Helmet>
-            <title>ImpactCMU</title>
-            <link rel="icon" type="image/png" sizes="32x32" href={ favicon16 } />
-            <link rel="icon" type="image/png" sizes="16x16" href={ favicon32 } />
-        </Helmet>
-        <Hero/>
-    </div>
-)
+import "../styles/fonts.css";
+import Landing2026 from "./Landing2026";
 
-export default IndexPage
+const IndexPage = () => (
+  <div>
+    <Helmet>
+      <title>ImpactCMU</title>
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+    </Helmet>
+    <Landing2026 />
+  </div>
+);
 
-// import React from "react";
-// import { Helmet } from "react-helmet";
-// import fav16 from '../components/2025img/16.svg';
-// import fav32 from '../components/2025img/32.svg';
-
-// export const onRenderBody = ({ setHeadComponents }) => {
-//     setHeadComponents([
-//         <Helmet key="helmet">
-//             <link rel="icon" type="image/png" sizes="32x32" href={fav32} />
-//             <link rel="icon" type="image/png" sizes="16x16" href={fav16} />
-//         </Helmet>
-//     ]);
-// };
+export default IndexPage;
