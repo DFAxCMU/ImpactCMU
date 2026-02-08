cf42c8ca14178910cdf459b56f51167adf32c971
Eunice <eylee2@andrew.cmu.edu>
2026-02-08 15:27:34 -0500

    font switching

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -2,15 +2,16 @@ import React from 'react';
 import schedule from '../components/2024img/Schedule.png';
 import submitline from '../components/2025img/submitline.svg';
 import NavBar from "../components/NavBar";
-import scarf from '../components/2025img/scarf.svg';
-import light from '../components/2025img/light.svg';
-import plushie from '../components/2025img/plushie.svg';
+import lamp from '../components/2026img/cat-lamp.png';
+import plush from '../components/2026img/cat-plush.png';
+import mug from '../components/2026img/cat-mug.png';
 
 import 'popper.js'
 import '../dist/css/bootstrap.min.css';
 import '../styles/style.css';
 import '../styles/page.css';
 import '../styles/fonts.css';
+import gradient from '../components/2026img/gradient-bg.png';
 
 const Hero = () => (
     <>
@@ -18,7 +19,7 @@ const Hero = () => (
         <section id="schedule">
             <div className="container-fluid">
                 <div className="text-center omega">
-                    <h4>EVENT SCHEDULE</h4>
+                    <h4>Event Schedule</h4>
                     <h5>April 3rd @ the UC (Danforth Lounge), 11-4 PM</h5>
                     {/* <div className="navbar-brand">
                         <img src={schedule} alt="title-img" class="title-img"/>
@@ -32,7 +33,7 @@ const Hero = () => (
                             <div className="box3">
                                 <h6>Lunch</h6>
                                 <h5 class = "time">11AM - 12PM</h5>
-                                <p>Enjoy free food and connect with other presenting teams.</p>
+                                <p class="bottom-align">Enjoy free food and connect with other presenting teams.</p>
                             </div>
                             <div className="box3">
                                 <h6>Kickoff Ceremony</h6>
@@ -61,16 +62,16 @@ const Hero = () => (
                     <center>
                         <div className="box4">
                             <h4>RAFFLE PRIZES</h4>
-                            <div class="bear">
-                                <div class="bearlist">
-                                    <p>1. Bear Scarf</p>
-                                    <p>2. Bear Light</p>
-                                    <p>3. Giant Bear Plushie</p>
+                            <div class="cat">
+                                <div class="catlist">
+                                    <p>1. Cat Lamp</p>
+                                    <p>2. Cat Plush</p>
+                                    <p>3. Cat Mug</p>
                                 </div>
-                                <div class="bearimg">
-                                    <img src={scarf} alt="title-img" class="cutie"/>  
-                                    <img src={light} alt="title-img" class="cutie"/> 
-                                    <img src={plushie} alt="title-img" class="cutie"/> 
+                                <div class="catimg">
+                                    <img src={lamp} alt="title-img" class="cutie" style={{width: "150px"}}/>  
+                                    <img src={plush} alt="title-img" class="cutie" style={{width: "150px"}}/> 
+                                    <img src={mug} alt="title-img" class="cutie" style={{width: "150px"}}/> 
                                 </div>
                             </div>
                         </div>
