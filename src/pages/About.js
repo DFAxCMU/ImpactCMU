2ebc8c6616fe11ecd159e57be02ce6a567e604b4
Karen Gonzalez <kareng@andrew.cmu.edu>
2026-02-08 17:32:04 -0500

    finished merging about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import who_are_we from '../components/2026img/who-are-we.svg';
 import what_is_impact from '../components/2026img/what-is-impact.svg';
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import aboutBg from "../components/2026img/about-background-gradient.png";
 import card from "../components/2026img/card.svg";
 import AboutCard from "../components/AboutCard";
+import gradient from "../components/2026img/gradient-bg.png";
+import group3 from "../components/2026img/group-3.svg";
 
 
 import 'popper.js';
@@ -34,7 +37,9 @@ const About = () => {
 
     return (
         <>
-            <img src={aboutBg} alt="background" className="background"/>
+            {/* <img src={aboutBg} alt="background" className="background"/> */}
+            <img className="bg-group-about" src={group3} alt="" />
+            <img className="bg-gradient-about" src={gradient} alt="" />
             <NavBar />
             {isSmallScreen ? (
                 <div className="container-about">
@@ -60,38 +65,40 @@ const About = () => {
                     </div>
                 </div>
             ) : (
-                <section id="about">
-                    <div className="container-about">
-                        {/* <div className="who_are_we-section"> */}
-                            <AboutCard 
-                                title="Who Are We?" 
-                                text="Impact CMU invites undergrad and graduate students at Carnegie Mellon to showcase how they have worked with a community. Student organizations and research, personal, and hackathon projects are all welcome to contribute their work!"
-                                imageSrc={who_are_we}
-                            />
-                            <AboutCard
-                                title="What is Impact?"
-                                text="You will be showcasing your work through a poster and optional demos. Attendees will view different teams, talk to you about your work, and vote to win prizes! Faculty judges will also take part in viewing work and discussing your creations! This is a great opportunity to talk to your peers, get feedback from your professors, and for student organizations to promote to a large audience!"
-                                imageSrc={what_is_impact}
-                            />
-                            <div className="logistics-section">
+                <>
+                <div className="about-page">
+                    <section id="about">
+                            <div className="container-about">
+                                {/* <div className="who_are_we-section"> */}
                                 <AboutCard
-                                    title="Where is it?"
-                                    text="April 3rd @ the UC (Danforth Lounge), 11-4 PM."
-                                />
-                                <AboutCard>
-                                    <>
-                                        <strong>Logistics:</strong> Chloe Xu, Sara Laman, Inseo Kim, Eunice Lee, Rebecca Sucgang<br/>
-                                        <strong>Communications:</strong> Beverly Zheng, Bach Ha Lan Vu, Tiffany Liu, Katie Wen<br/>
-                                        <strong>Design:</strong> Kyla Anderson, Bach Ha Lan Vu, Beverly Zheng, Katie Wen<br/>
-                                        <strong>PR:</strong> Sulli Yatabe<br/>
-                                        <strong>Tech:</strong> Karen Gonzalez, Saanvi Kandala, Inseo Kim, Helen Zhang, Eunice Lee<br/>
-                                        <strong>Finance:</strong> Amanda Witt<br/>
-                                        <strong>Management:</strong> Anna Mathews, Sophie Hao, Gryphon Chong
-                                    </>
-                                </AboutCard>
-                            </div> 
-                    </div>
-                </section>
+                                    title="Who Are We?"
+                                    text="Impact CMU invites undergrad and graduate students at Carnegie Mellon to showcase how they have worked with a community. Student organizations and research, personal, and hackathon projects are all welcome to contribute their work!"
+                                    imageSrc={who_are_we} />
+                                <AboutCard
+                                    title="What is Impact?"
+                                    text="You will be showcasing your work through a poster and optional demos. Attendees will view different teams, talk to you about your work, and vote to win prizes! Faculty judges will also take part in viewing work and discussing your creations! This is a great opportunity to talk to your peers, get feedback from your professors, and for student organizations to promote to a large audience!"
+                                    imageSrc={what_is_impact} />
+                                <div className="logistics-section">
+                                    <AboutCard
+                                        title="Where is it?"
+                                        text="April 3rd @ the UC (Danforth Lounge), 11-4 PM." />
+                                    <AboutCard>
+                                        <>
+                                            <strong>Logistics:</strong> Chloe Xu, Sara Laman, Inseo Kim, Eunice Lee, Rebecca Sucgang<br />
+                                            <strong>Communications:</strong> Beverly Zheng, Bach Ha Lan Vu, Tiffany Liu, Katie Wen<br />
+                                            <strong>Design:</strong> Kyla Anderson, Bach Ha Lan Vu, Beverly Zheng, Katie Wen<br />
+                                            <strong>PR:</strong> Sulli Yatabe<br />
+                                            <strong>Tech:</strong> Karen Gonzalez, Saanvi Kandala, Inseo Kim, Helen Zhang, Eunice Lee<br />
+                                            <strong>Finance:</strong> Amanda Witt<br />
+                                            <strong>Management:</strong> Anna Mathews, Sophie Hao, Gryphon Chong
+                                        </>
+                                    </AboutCard>
+                                </div>
+                            </div>
+                        </section>
+                        <Footer />
+                </div>
+                </>
             )}
         </>
     );
